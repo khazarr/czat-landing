5840eecf127e637686ab20586fbd865ae01ce776
Karol Swieca <swieca.karol@gmail.com>
2018-03-10 13:33:46 +0100

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+
+    <v-toolbar color="elevation-0" fixed app>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+        <v-spacer />  
+        <v-btn flat color="primary">SIGN UP</v-btn>
+        <v-btn flat color="primary">PRICING</v-btn>
+        <v-btn flat color="primary">ABOUT</v-btn>
+        <v-btn flat color="primary">LOGIN</v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Cool chat'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-layout column justify-center align-center justify-space-around>
+    <v-flex xs12 sm8 md6>
+     
+      <v-card class="card-centered">
+         <div class="text-xs-center">
+            <img src="/golab.png" alt="Vuetify.js" class="mt-3 mb-1 logo" />
+         </div>
+        <v-card-title class="headline text-xs-center">ePidgeon</v-card-title>
+        <v-card-text>
+          <p>Odwiedzający stronę czekają na Twoje wsparcie</p>
+          <p>Czat to najszybsza forma komunikacji z klientem - jest znacznie szybszy niż e-mail i dużo bardziej efektywy niż telefon. Nie każ swoim klientom czekać!</p>
+          <h3 class="mt-3">Ten czat to sekret mojego sukcesu</h3>
+          <div class="text-xs-right">
+            <em><small>&mdash; Mr. Succes</small></em>
+          </div>
+          <hr class="my-3">
+          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
+          <br>
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style scoped>
+  .card-centered{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 600px;
+    min-width: 500px;
+   }
+
+   .logo {
+     max-width: 200px;
+   }
+
+   .headline {
+     display: block;
+   }
+</style>
+
